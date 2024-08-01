4e8ff2f470a01b08aab50ed163f7126225f22ee2
xx-hmi <165568150+xx-hmi@users.noreply.github.com>
2024-08-01 22:56:48 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $product_date = ['name' => 'onion', 'price' => 200, 'weignt' => 160];
+    print_r($product_date);
+    ?>
+  </p>
+</body>
+</html>
